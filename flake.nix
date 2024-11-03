17f7e785555660892b5ce6b06ecf506fe5325e31
Leo Qin <leozqin@gmail.com>
2024-11-02 18:21:43 -0700

    feat: add post about NixOS

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Leo's Blog";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      name = "blog";
+      packages = with pkgs; [
+        hugo
+      ];
+    };
+  };
+}
