3d83e3cd43fb680235e941667bbd0f55d6c5754d
Leo Qin <leozqin@gmail.com>
2025-02-12 18:37:01 -0800

    feat: spacing on ultrawide screens

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,6 +13,12 @@ export default {
 		backgroundColor: {
 			primary: "#1a1b26",
 			code: "#0a0c10" // github dark high contrast
+		},
+		screens: {
+			"sm": "640px",
+			"md": "768px",
+			"lg": "1024px",
+			"xl": "2000px"
 		}
 	},
 	plugins: [require('@tailwindcss/typography')],
